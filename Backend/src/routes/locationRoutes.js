4c05570b157f13756b0e54533161086c84269e08
Nashonlyy-dev <durgaysubedi3@gmail.com>
2026-02-28 10:15:41 +0630

    Delete Backend directory

--- Backend/src/routes/locationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { requestLocationUpdate } from '../controllers/locationController.js';
-import { protect, isOwner } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.post('/update', protect, requestLocationUpdate);
-
-export default router;
